af89c33d7a3576a7087fd64ebe55f7c3b56cc40e
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-01 02:21:40 +0200

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argv_length = len(argv)
+    argn = "argument" if argv_length == 2 else "arguments"
+    delm = "." if argv_length == 1 else ":"
+    print("{:d} {:s}{:s}".format(argv_length - 1, argn, delm))
+    for i, arg in enumerate(argv):
+        if i > 0:
+            print("{:d}: {:s}".format(i, arg))
