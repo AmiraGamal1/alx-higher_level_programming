861033ed2d7c0d985e060801202d5cedaf6df9dd
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-04 00:00:50 +0200

    commit all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argv_length = len(argv)
+    argn = "argument" if argv_length == 2 else "arguments"
+    delm = "." if argv_length == 1 else ":"
+    print("{} {:s}{:s}".format(argv_length - 1, argn, delm))
+    for i in range(1, argv_length):
+        print("{}: {}".format(i, argv[i]))
