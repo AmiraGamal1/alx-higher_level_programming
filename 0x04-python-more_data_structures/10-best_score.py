73f3633204f9f79145b7d89c536dae3305599beb
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-06 22:52:33 +0200

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary and len(a_dictionary):
-        sort_dict = sorted(a_dictionary.keys())
+        sort_dict = sorted(list(a_dictionary.keys()))
         return sort_dict[-1]
     return None
