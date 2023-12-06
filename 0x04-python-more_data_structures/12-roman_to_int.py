d479ec9b36ae26f54a89e9591a2c05f9db40eaa0
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-07 00:02:03 +0200

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10,
+                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    number = 0
+    i = 0
+    if roman_string and isinstance(roman_string, str):
+        while i < len(roman_string) - 1:
+            if roman_dict[roman_string[i]] >= roman_dict[roman_string[i + 1]]:
+                number += roman_dict[roman_string[i]]
+            else:
+                number -= roman_dict[roman_string[i]]
+            i += 1
+        number += roman_dict[roman_string[i]]
+        return number
+    return 0
