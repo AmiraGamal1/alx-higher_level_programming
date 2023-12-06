5fc555de9fc5344320131062a4c36caf3bbe1847
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-07 00:16:52 +0200

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        res = 0
+        weight = 0
+        for item in my_list:
+            res += item[0] * item[1]
+            weight += item[1]
+        return (res / weight)
+    return (0)
