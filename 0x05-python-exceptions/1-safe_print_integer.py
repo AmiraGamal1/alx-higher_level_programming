b71e551b41ec69e25b19f98b27e9d0990e9a1aa3
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-20 11:12:11 +0200

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+    """Print integer
+    Args:
+        value (int): the value to be print
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
