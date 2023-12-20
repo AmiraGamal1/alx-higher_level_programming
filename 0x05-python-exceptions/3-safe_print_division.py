7a8bc0683c02a128aa8f66c60b9598900311430c
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-20 11:41:23 +0200

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """safe division
+
+    Args:
+         a (int): int value
+         b (int): int value
+    Returns:
+         division result or None
+    """
+    try:
+        res = a / b
+    except (TypeError, ZeroDivisionError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return (res)
