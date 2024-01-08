773da581c144e96bf5b636d2c11fb7303515f5dd
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-08 13:19:54 +0200

    5-base_geometry

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Define inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """ inherits_from function
+    Args:
+        obj (object): object
+        a_class: class name
+    Return: Ture if obj is a_class False else
+    """
+    return  isinstance(obj, a_class)
--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Define BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    pass
