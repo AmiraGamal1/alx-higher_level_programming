b11f7e84f141f21f7e0bf5167753bb9f4bd0b471
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-08 15:38:33 +0200

    all task

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        Rectangle.__init__(self, size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        Rectangle.__init__(self, size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+    
+    def __str__(self):
+        string = "[" + str(__class__.__name__) + "]"
+        string += " " + str(self.__size) + "/" + str(self.__size)
+        return string
+    
