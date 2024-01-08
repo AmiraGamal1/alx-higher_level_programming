d7c0f1b42a6b0d76f8d5b7b3a061ba3633baa06e
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-08 12:39:14 +0200

    is instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Define is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """check if obj is instance of a_class"""
+    return (type(obj) is a_class)
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Define is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if obj is instance of a_class and its parent"""
+    return isinstance(obj, a_class)
