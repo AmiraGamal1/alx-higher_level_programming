2c39cbb3ac9db1edeaeee1f92591fc253c77fd30
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-09 12:06:32 +0200

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Define from json string"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """convert json to python object
+    Args:
+        my_str: json string
+    Return: python object
+    """
+    return json.loads(my_str)
