bfea5571fa40d0707344b5812e52d3bdc3e46cd3
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-09 11:40:46 +0200

    2-append_wirte.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Defines append_wirte function"""
+
+
+def append_write(filename="", text=""):
+    """append to a file
+    Args:
+        filename: utf-8 file
+        text: text to append to a file
+    Return: number of character added
+    """
+
+    with open(filename, 'a', encoding="utf-8") as file:
+        return file.write(text)
