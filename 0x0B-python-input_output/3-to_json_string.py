240725cd90ca1a84f52f017067da0fbd2fc3cf5d
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-09 11:59:18 +0200

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Define to_json_string"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """convert object to json
+    Args:
+        my_obj: object
+    Return: json representaion"""
+    return json.dumps(my_obj)
