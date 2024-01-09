789f1cd14297ab53c2f4a870ff3c192d63d54965
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-09 11:28:37 +0200

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Defines write_file"""
+
+
+def write_file(filename="", text=""):
+    """write to a file"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
