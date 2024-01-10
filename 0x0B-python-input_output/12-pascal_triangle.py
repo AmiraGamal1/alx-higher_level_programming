b295711ac0a66efb8309c5bbdae5a2b312dae11a
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-10 15:53:25 +0200

    pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Pascal Triangle"""
+
+
+def pascal_triangle(n):
+    """create pascal triangle"""
+    if n <= 0:
+        return []
+    pascal = [[1]]
+    while (len(pascal) < n):
+        row = [1]
+        for i in range(1, len(pascal[-1])):
+            row.append(pascal[-1][i - 1] + pascal[-1][i])
+        row.append(1)
+        pascal.append(row)
+    return pascal
