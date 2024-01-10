a0fa9bc217393e74be834b0f9dd94f24eef7875f
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-10 12:27:28 +0200

    7- fix error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
     try:
-        items = load_from_json_file("my_list.json")
-    except FileExistsError:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "my_list.json")
+    save_to_json_file(items, "add_item.json")
