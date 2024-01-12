797854eff9f526ad5e89d4b1afb12d216627e4af
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-01-12 11:28:13 +0200

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""base class to manage instance id"""
+
+
+class Base:
+    """base class model
+    Private class attribute: (int) __nb_objects
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+        Args:
+            id (int): the identity of the new instance
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
