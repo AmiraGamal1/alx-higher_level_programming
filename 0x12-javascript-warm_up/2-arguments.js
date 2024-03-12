c3946ba31071b4fb991d1c3b2215f95049060265
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-12 20:25:44 +0200

    fix error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const process = require('process');
 if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('No argument');
+  console.log('Arguments found');
 } else {
   console.log('Arguments found');
 }
