71c4e0854e69204f311f7787e5b1a3591d496c3f
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-12 22:37:21 +0200

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const process = require('process');
+const num = parseInt(process.argv[2]);
+function factorial (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
