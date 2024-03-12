23782e353bf0c467e5991f4b7a0f094027ec16dd
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-12 22:59:46 +0200

    second biggest integer

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const num = process.argv.slice(2);
+if (num.length === 0 || num.length === 1) {
+  console.log(0);
+} else {
+  num.sort();
+  num.reverse();
+  console.log(num[1]);
+}
