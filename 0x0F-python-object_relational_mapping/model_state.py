85df303674b5bc6e995cdf4c95e69d66546f06f0
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-18 12:28:28 +0200

    fix errot

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,10 +4,12 @@ Create states table
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
 
 class State(Base):
+    """State class"""
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, primary_key=True,
                 nullable=False, unique=True)
