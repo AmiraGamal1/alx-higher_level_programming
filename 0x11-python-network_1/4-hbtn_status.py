49ba35b148ba2c19f8326f3b46e13e2a94187da6
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-29 01:49:37 +0200

    requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""fetches url use requests"""
+import requests
+
+
+def fetch():
+    """fetch url"""
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
+
+
+if __name__ == '__main__':
+    fetch()
