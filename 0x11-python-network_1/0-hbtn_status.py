82b7c75329f7dc81cb522a9c0e983d2a445ca011
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-28 13:14:07 +0200

    fix error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""fetch url"""
+import urllib.request
+
+
+def fetch():
+    """print fetched url"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as url:
+        r = url.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode('utf8')))
+
+
+if __name__ == "__main__":
+    fetch()
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""sends a request to the URL"""
+import urllib.request
+import sys
+
+
+def send():
+    """sends a request to the url"""
+    with urllib.request.urlopen(sys.argv[1]) as req:
+        x_request_id = req.getheader('X-Request-Id')
+        print(x_request_id)
+
+
+if __name__ == "__main__":
+    send()
