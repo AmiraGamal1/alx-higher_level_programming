d05fe3b2718b85ab3cedbcf61a939b5125b81e5a
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-29 01:25:22 +0200

    handle error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+sends a request to the URL and displays the body of the
+response
+"""
+import sys
+from urllib import request, error
+
+
+def send():
+    """send a request to the url and handle error"""
+    try:
+        with request.urlopen(sys.argv[1]) as req:
+            res = req.read().decode('utf-8')
+            print(res)
+    except error.HTTPError as e:
+        print('Error code: ' + str(e.code))
+
+
+if __name__ == '__main__':
+    send()
