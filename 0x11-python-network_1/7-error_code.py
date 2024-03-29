4d4792670a156fc97a4c763026408bd68cc2f4d7
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-29 02:33:01 +0200

    error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""handle error code"""
+import requests
+import sys
+
+
+def send():
+    """send request"""
+    req = requests.get(sys.argv[1])
+    if req.status_code == requests.codes.ok:
+        print(req.text)
+    else:
+        print("Error code: {}".format(req.status_code))
+
+
+if __name__ == "__main__":
+    send()
