b91d19d387e3780a2d689f04641c8d8aeef889d3
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-29 02:22:02 +0200

    post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""post an email"""
+import requests
+import sys
+
+
+def send():
+    """post an email"""
+    req = requests.post(sys.argv[1], data={"email": sys.argv[2]})
+    print(req.text)
+
+
+if __name__ == '__main__':
+    send()
