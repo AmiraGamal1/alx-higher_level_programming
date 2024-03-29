a1c39026e32a4214a78f5eb907e628da8eaec095
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-29 02:12:42 +0200

    id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""sends a request to the URL and displays the
+value of the variable X-Request-Id"""
+import requests
+import sys
+
+
+def send():
+    """print X-Request-Id"""
+    res = requests.get(sys.argv[1])
+    id = res.headers.get('X-Request-Id')
+    print(id)
+
+
+if __name__ == '__main__':
+    send()
